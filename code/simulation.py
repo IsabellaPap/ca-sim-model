#1 adjust grid model (add Cell class) and fix P_s calculation

code/simulation.py
@@ -6,7 +6,7 @@ Created: Wed 14 Jun, 2023
 
 # imports
 from enum import IntEnum
-from math import cos, exp, sqrt, tan, pi
+from math import cos, exp, sqrt, tan, pi, atan
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
@@ -71,13 +71,21 @@ class Grid():
           for j in range(len(self.grid[i])):
               # Change how the grid is populated
               grid_rep[i][j] = np.random.choice(3,1,p=[0,p_nofuel,1-p_nofuel])
+              
               if grid_rep[i][j] == 1:
-                self.grid[i][j] = noFuel()
-                noFuel.setAltitude(self.grid[i][j],self,x=i+1,y=j+1)
+                pveg = 0
+                pden = 0
+                state = State.NO_FUEL
 
               else:
-                self.grid[i][j] = Fuel(np.random.choice([-0.3,0,0.4],1),np.random.choice([-0.4,0,0.3],1))
-                Fuel.setAltitude(self.grid[i][j],self,x=i+1,y=j+1)
+                pveg = np.random.choice([1,2,3],1)
+                pden = np.random.choice([1,2,3],1) 
+                state = State.NOT_BURNING
+
+              # initialise Cell
+              self.grid[i][j] = Cell(pveg,pden,state)
+
+              Cell.setAltitude(self.grid[i][j],self,x=i,y=j)
 
       self.ShowGrid('dem')
     """Description
@@ -105,34 +113,33 @@ class Grid():
       grid_rep_dem = np.zeros((self.height,self.width), dtype=float)
       normalized_grid = np.zeros((self.height,self.width), dtype=float)
 
-      if colormap == 'sim':
-        unique_states = set()
-        for i in range(len(self.grid)):
-            for j in range(len(self.grid[i])):
-                grid_rep_sim[i][j] = (self.grid[i][j].state.value)
-                unique_states.add(self.grid[i][j])
-        num_states = len(unique_states)
-        colormap_to_use = colors.ListedColormap(["grey","green","red","black"][:num_states])
-        plt.figure(figsize=(10,10))
-        plt.imshow(grid_rep_sim,cmap=colormap_to_use)
-      elif colormap == 'dem':
-         for i in range(len(self.grid)):
-            for j in range(len(self.grid[i])):
-                if isinstance(self.grid[i][j],noFuel):
-                  grid_rep_dem[i][j] = (noFuel.getAltitude(self.grid[i][j]))
-                else:
-                   grid_rep_dem[i][j] = (Fuel.getAltitude(self.grid[i][j]))
-         min_altitude = np.min(grid_rep_dem)
-         max_altitude = np.max(grid_rep_dem)
-         colormap_to_use = 'Greens'
-         normalized_grid = (grid_rep_dem - min_altitude) / (max_altitude - min_altitude)
+      plt.figure(figsize=(10,10))
 
-         plt.figure(figsize=(10,10))
-         plt.imshow(normalized_grid,cmap=colormap_to_use)
+      #DEM Colormap
+      for i in range(len(self.grid)):
+        for j in range(len(self.grid[i])):
+          grid_rep_dem[i][j] = (Cell.getAltitude(self.grid[i][j]))
+      min_altitude = np.min(grid_rep_dem)
+      max_altitude = np.max(grid_rep_dem)
+      normalized_grid = (grid_rep_dem - min_altitude) / (max_altitude - min_altitude)
+      plt.imshow(normalized_grid,cmap='Greens')
+
+      # SIM Colormap
+      unique_states = set()
+      for i in range(len(self.grid)):
+          for j in range(len(self.grid[i])):
+              grid_rep_sim[i][j] = (self.grid[i][j].state.value)
+              unique_states.add(self.grid[i][j])
+      transparent_green = (0, 1, 0, 0)
+      colormap = colors.ListedColormap([transparent_green,"red","black"])
+      plt.imshow(grid_rep_sim,cmap=colormap,alpha=0.9)
+
+      
       
       plt.title('Grid Visualization (Time Step = {})'.format(iter_num))
       plt.grid(False)
       plt.savefig('./output/{}.png'.format(10 + iter_num))
+      plt.close()
     
     """Description
     Parameters
@@ -204,8 +211,8 @@ class Grid():
                 else:
                    position = 1
                 p_burn = Grid.calculatePburn(self,self.grid[x][y],self.grid[x+i-1][y+j-1], position)
-                if p_burn > 0:
-                  self.ignite(x+i-1,y+j-1)
+                if np.random.rand() < p_burn:
+                    self.ignite(x+i-1,y+j-1)
 
                  
         """Description
@@ -239,34 +246,39 @@ class Grid():
       # enforce rule 2: if a cell is in state 3 - BURN, it will be BURNED down in the next time step
       for cell in burning_cells:
         x,y = cell
+        # if x==self.width/2 and y ==self.height/2:
+        #   print(burn_count)
+        # if x==self.width and y==self.height:
+        #   print(burn_count)
         self.grid[x][y].setState(State.BURNED)
 
       self.ShowGrid('sim',burn_count)
 
     def calculatePburn(self, cell_from, cell_to, position):
       P_h = 0.58
-      p_veg, p_den = Fuel.getFuelAttributes(cell_to)
+      p_veg, p_den = Cell.getFuelAttributes(cell_to)
 
       # c1 = 0.045
       # c2 = 0.131
       # ft = exp(V*c2*(cos(theta)-1))
       # P_w = exp(c1*V)*ft
 
-      a = 0.078
+      a = 0.9
       # cell altitudes
-      E1 = Fuel.getAltitude(cell_from)
-      E2 = Fuel.getAltitude(cell_to)
+      E1 = Cell.getAltitude(cell_from)
+      E2 = Cell.getAltitude(cell_to)
       l = 25
       if E1 - E2 == 0:
          theta_s = 0
       else:  
         if position == 0:
-          theta_s = pow(tan(E1-E2/l),-1)
+          theta_s = atan((E1 - E2) / l)
         elif position == 1:
-          theta_s = pow(tan(E1-E2/sqrt(2*l)),-1)
+          theta_s = atan((E1 - E2) / (l * sqrt(2)))
       P_s = exp(a*theta_s)
 
       P_burn = P_h*(1+p_veg)*(1+p_den)*P_s
+
 
       return P_burn
 
@@ -275,41 +287,64 @@ class Grid():
 
       
 
-class Fuel():
-   
-  def __init__(self,pveg,pden):
+class Cell():
+  """Description
+    Parameters
+    ---------
+    self: Cell
+    the Cell to initialise
+
+    pveg: float
+    discrete value of the vegetation type
+
+    pden: float
+    discrete value of the vegetation density
+
+    state: IntEnum
+    the state in which the cell is initialised in
+
+    Functionality
+    ---------
+    it initialises the Cell with given FuelAttributes and state
+    
+
+    """
+  def __init__(self,pveg,pden,state):
       self.pveg = pveg
       self.pden = pden
-      self.state = State.NOT_BURNING
-    
+      self.state = state
+
+  def getState(self):
+    return self.state
+  
   def setState(self,state):
      self.state = state
 
   def getFuelAttributes(self):
      return self.pveg,self.pden
   
+  def setFuelAttributes(self,pveg,pden):
+     self.pveg= pveg;
+     self.pden= pden;
+  
   def getAltitude(self):
      return self.altitude
-  def setAltitude(self,grid,x,y):
-     center_x = grid.width / 2
-     center_y = grid.height / 2
-     distance_from_center = sqrt((x - center_x)**2 + (y - center_y)**2)
-     sigma = 0.000006
-     self.altitude = (1/(2*pi*sigma**2))*exp(-(distance_from_center**2)/2*sigma**2)
-     
-class noFuel():
-  def __init__(self):
-        self.state = State.NO_FUEL
+ 
+  
+  def setAltitude(self, grid, x, y):
+    mu_x = grid.width / 2
+    mu_y = grid.height / 2
+    sigma_x = grid.width / 6
+    sigma_y = grid.height / 6
+    A=0
+    altitude = A * exp(-(((x - mu_x) ** 2) / (2 * sigma_x ** 2) + ((y - mu_y) ** 2) / (2 * sigma_y ** 2)))
+    self.altitude = altitude
 
-  def setAltitude(self,grid,x,y):
-     center_x = grid.width / 2
-     center_y = grid.height / 2
-     distance_from_center = sqrt((x - center_x)**2 + (y - center_y)**2)
-     sigma = 0.000006
-     self.altitude = (1/(2*pi*sigma**2))*exp(-(distance_from_center**2)/2*sigma**2)
+    if x==mu_x and y ==mu_y:
+      print(self.altitude)
 
-  def getAltitude(self):
-     return self.altitude
+
+
 
 # modules
 
@@ -321,8 +356,8 @@ if __name__ == "__main__":
   grid = Grid(height,width)
   count = 1
 
-  grid.PopulateGrid(0.02)
-  grid.ignite(1,3)
-  for i in range(25):
+  grid.PopulateGrid(0)
+  grid.ignite(1,1)
+  for i in range(50):
     grid.ca_simulation(count)
     count += 1